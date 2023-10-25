724d6c472c450941c1681db8e7efe8c898a874d3
CulturalMass <melherrra@hotmail.com>
2023-10-25 19:17:17 +0200

    Adding the styles file

---
+++ src/styles/index.js
@@ -0,0 +1,31 @@
+export const styles = {
+  //pages-HOME
+  bodyContainer:
+    "overflow-y-auto min-h-[40.625rem] max-w-[25rem] sm:max-w-[75rem] 2xl:min-w-[121.875rem] ml-[4.688rem] 2xl:ml-[10.625rem] mt-[7vh] sm:[9vh]",
+  contentWrapper: "flex flex-col -ml-[8.313rem] sm:ml-[0.625rem] text-white",
+
+  //components-DATEPICKER
+  datePickerWrapper:
+    "absolute flex top-[6.4%] 2xl:top-[4.9rem] h-12 max-w-[18.5rem] sm:min-w-[75.1rem] 2xl:min-w-[121.8rem] overflow-hidden sm:overflow-hidden text-white border-t-[0.063rem] border-[#505050]",
+
+  //components-DISPLAYHOURS
+  displayHoursContainer:
+    "relative flex flex-col items-center justify-center align-middle h-10 text-[1.375rem] border-t-[0.063rem] border-[#505050]",
+
+  //components-MODAL
+  modalWrapper:
+    "absolute h-[52.6rem] sm:flex sm:flex-row w-[24.4rem] sm:h-4/6 sm:w-4/6 sm:top-[15%] sm:left-[15%] bg-[#393939] items-center align-middle justify-center text-white z-50 overflow-hidden sm:rounded-lg sm:border-[0.05rem]",
+  modalNavBar:
+    "fixed flex flex-row justify-between items-center top-[1%] sm:top-[17%] w-[24rem] sm:w-[59.5rem]",
+  channelLogoContainer:
+    "flex max-w-[7rem] sm:min-w-[8rem] sm:mr-[2rem] sm:-mt-6 justify-center align-middle max-h-16",
+
+  //components-SCROLLBTN
+  btn: "fixed cursor-pointer bg-[#393939] left-[70%] sm:left-[75%] mt-[32.813rem] z-10 w-20 h-12 text-[1.5rem] font-bold rounded-2xl drop-shadow-[2px_2px_2px_rgba(255,255,255,1)] hover:bg-[#242424] hover:drop-shadow-[2px_2px_5px_rgba(255,255,255,1)] hover:w-[5.063rem]",
+
+  //Components-SIDEBARLIST
+  startIconContainer:
+    "flex -mt-[3.0rem]  min-w-[5rem] sm:min-w-[10rem] 2xl:min-w-[15rem] h-12 z-30 bg-[#202020] justify-center align-middle border-[0.063rem] border-[#505050] rounded-md drop-shadow-[1px_1px_0px_rgba(255,255,255,1)]",
+  channelContainer:
+    "flex max-w-[5rem] sm:max-w-[10rem] 2xl:max-w-[12rem] h-16 bg-[#202020] justify-center align-middle border-[0.063rem] border-[#505050] rounded-md drop-shadow-[2px_2px_0px_rgba(255,255,255,1)]",
+};
